8ebcef912888b5ce7a9f6377cc634689c4a414cb
Miguel Mendes <150489109+MiguelMendes01@users.noreply.github.com>
2025-07-17 11:32:36 -0300

    Add files via upload
    Pega 3 números inseridos pelo usuário e verifica se estão em ordem crescente ou não.

---
+++ verificando ordenaçao.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Insira um número inteiro: "))
+numero2 = int(input("Insira um número inteiro: "))
+numero3 = int(input("Insira um número inteiro: "))
+
+if numero1 < numero2:
+    if numero2 < numero3:
+        print("crescente")
+else:
+    print("não está em ordem crescente")
