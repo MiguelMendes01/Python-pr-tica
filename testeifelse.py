3c2c81945f48aba2d4c1d498e6c7226042e2e3d9
Miguel Mendes <150489109+MiguelMendes01@users.noreply.github.com>
2025-08-13 11:45:42 -0300

    Update testeifelse.py

--- testeifelse.py
+++ testeifelse.py
@@ -10,14 +10,19 @@ if delta >= 0:
     bhaskara1 = (-b + math.sqrt(delta)) / (2 * a)
     bhaskara2 = (-b - math.sqrt(delta)) / (2 * a)
     if bhaskara1 < bhaskara2:
-        print("as raízes da equação são", bhaskara2, bhaskara1)
+        print("as raízes da equação são", bhaskara2,"e", bhaskara1)
     if bhaskara2 < bhaskara1:
-        print("as raízes da equação são", bhaskara1, bhaskara2)
+        print("as raízes da equação são", bhaskara1,"e", bhaskara2)
     if delta == 0:
         print("a raiz desta equação é", bhaskara1)
 
 else:
     print("esta equação não possui raízes reais")
+
+
+
+
+
 
 
 
